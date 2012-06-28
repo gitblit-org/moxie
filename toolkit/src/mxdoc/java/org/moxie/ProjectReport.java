7a9ce5d8622caeffea25c3f2164264f4f468471b
James Moger <james.moger@gmail.com>
2012-06-27 21:47:52 -0400

    If no source folders, do not include source folders in project report

--- toolkit/src/mxdoc/java/org/moxie/ProjectReport.java
+++ toolkit/src/mxdoc/java/org/moxie/ProjectReport.java
@@ -31,13 +31,15 @@ public class ProjectReport implements MoxieReport {
 		sb.append("</table>\n");
 
 		// source folders
-		sb.append("<p />\n");
-		sb.append(format(h2Pattern,"source folders"));
-		sb.append("<table class='table'>\n");
-		for (SourceFolder sourceFolder : build.getSourceFolders()) {
-			sb.append(format(kvpPattern, sourceFolder.name, sourceFolder.scope));	
+		if (build.getSourceFolders().size() > 0) {
+			sb.append("<p />\n");
+			sb.append(format(h2Pattern,"source folders"));
+			sb.append("<table class='table'>\n");
+			for (SourceFolder sourceFolder : build.getSourceFolders()) {
+				sb.append(format(kvpPattern, sourceFolder.name, sourceFolder.scope));	
+			}
+			sb.append("</table>\n");
 		}
-		sb.append("</table>\n");
 		return sb.toString();
 	}
 	
