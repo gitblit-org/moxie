26033b8d47f8edb0e9f94fcfb2c45e71b0a9bba8
Uli Heller <uli.heller@daemons-point.com>
2013-09-15 09:37:51 +0200

    Added a gradle build
    With the current moxie master, the build doesn't succeed.
    Do this to execute the gradle build:
    
     1. Start the moxie proxy
       cd proxy
       rm -rf moxie
       ant run
    
     2. Start the gradle build
       cd src/test/gradle/010-slf4j
       rm -rf ../gradle-user-home
       gradle --gradle-user-home $(pwd)/../gradle-user-home test
    
    On linux, there is a script doing all of this:
    
       ./src/test/gradle/run-gradle-tests.sh
    
    The build "gradle test" hangs here:
    
       ...
       :compileJava
       Download http://repo1.maven.org/maven2/org/slf4j/slf4j-api/1.7.5/slf4j-api-1.7.5.pom
       Download http://repo1.maven.org/maven2/org/slf4j/slf4j-parent/1.7.5/slf4j-parent-1.7.5.pom
       > Building > :compileJava > Resolving dependencies ':compile'
    
    The output of "cd proxy; ant run" is this:
    
       ...
       [mx:run] Download status: 200
       [mx:run] Content: 2689 bytes; text/xml
       [mx:run] Got request for http://repo1.maven.org/maven2/org/slf4j/slf4j-parent/1.7.5/slf4j-parent-1.7.5.pom
       [mx:run] Downloading to /home/uli/git/moxie/proxy/moxie/remote/repo1.maven.org_maven2/org/slf4j/slf4j-parent/1.7.5/slf4j-parent-1.7.5.pom
       [mx:run] Download status: 200
       [mx:run] Content: 11804 bytes; text/xml
       [mx:run] Found no URL to download in request:
       [mx:run] HEAD http://repo1.maven.org/maven2/org/slf4j/slf4j-parent/1.7.5/slf4j-parent-1.7.5.jar HTTP/1.1
       [mx:run] Accept-Encoding: gzip,deflate
       [mx:run] Host: repo1.maven.org
       [mx:run] Proxy-Connection: Keep-Alive
       [mx:run] User-Agent: Gradle/1.8-rc-1 (Linux;3.11.0-031100-generic;amd64) (Sun Microsystems Inc.;1.6.0_27;20.0-b12)
       [mx:run]
       [mx:run] indexing moxie/remote/repo1.maven.org_maven2/org/slf4j/slf4j-api/1.7.5/slf4j-api-1.7.5.pom
       [mx:run] indexing moxie/remote/repo1.maven.org_maven2/org/slf4j/slf4j-parent/1.7.5/slf4j-parent-1.7.5.pom
    
    A patch for the issue will follow soon. I do have one available, I just
    have to do some cleanup on it...

---
+++ proxy/src/test/gradle/run-gradle-tests.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+#
+# Run some tests based on moxie proxy and gradle.
+# Make sure that:
+# - you're able to run moxie proxie, i.e. the required tcp ports aren't in use
+# - you don't run jetty at the moment - it will be killed by this script :(
+# - you have gradle available and you can run it via "gradle" (I'm using 1.8.-rc-1 at the moment)
+#
+# I've tested this script on Linux only.
+#
+
+D="$(dirname "$0")"
+D="$(cd "${D}"; pwd)"
+
+BN="$(basename "$0")"
+
+GRADLE_CACHE="${D}/gradle-user-home"
+MOXIE_PROXY="${D}/../../.."
+MOXIE_PROXY_CACHE="${MOXIE_PROXY}/moxie"
+MOXIE_PROXY_OUTPUT="${D}/moxie-proxie.output.txt"
+
+# clean the moxie proxy cache
+rm -rf "${MOXIE_PROXY_CACHE}"
+
+killJetty () {
+  ps -ao pid,command|grep jetty|grep -v grep|cut -d' ' -f1|xargs -r kill -9
+}
+
+cleanGradleCache () {
+  rm -rf "${GRADLE_CACHE}"
+  find "${D}" -name .gradle|xargs -r rm -rf
+}
+
+cleanGradleBuilds () {
+  find "${D}" -name build -o -name "*.log"|xargs -r rm -rf
+}
+
+killJetty
+
+# compile the moxie proxy
+(
+cd "${MOXIE_PROXY}"
+ant
+)
+
+# run the moxie proxy
+(
+cd "${MOXIE_PROXY}"
+ant run
+) >"${MOXIE_PROXY_OUTPUT}" 2>&1  &
+
+# clean the gradle cache
+cleanGradleCache
+
+RC=0
+for d in "${D}"/*; do
+  if [ -d "${d}" ]; then
+    (
+      cd "${d}"
+      gradle --gradle-user-home "${GRADLE_CACHE}" test
+    )
+    _RC=$?
+    if [ ${RC} -eq 0 ]; then
+	RC="${_RC}"
+    fi
+  fi
+done
+
+rm -f "${MOXIE_PROXY_OUTPUT}"
+killJetty
+cleanGradleCache
+cleanGradleBuilds
+
+if [ ${RC} -ne 0 ]; then
+  echo "${BN}: FAILURE, rc=${RC}"
+fi
+exit "${RC}"
